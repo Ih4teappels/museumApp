e7b35512d67c7359e9203c5faa298338f1303635
Ih4teappels <kev10@live.nl>
2015-11-23 12:05:51 +0100

    small update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 window.addEventListener("load",function(){
+
+	var codes = ["1","2","3","4"];
+
+
+
  $('#reader').html5_qrcode(function(data){
-        console.log("GREAGESES SUCCESES");
-        console.log("GREAHT KAZAGSTAN GOTTET : " + data);
+        console.log("Scan Succes! : " + data);
     },
     function(error){
         console.log("error : " + error);
