b42cddb8b1e5e77d864ebc9b5475d02105526f01
Robinwist <robinwist@hotmail.com>
2015-11-30 12:05:52 +0100

    ja

--- js/artwork.js
+++ js/artwork.js
@@ -1,3 +1,21 @@
 /**
  * Created by Robin on 30-11-2015.
  */
+
+var artwork = [
+    {
+        id: 3,
+        name: 'Nachtwatch',
+        img_src: 'http://lh3.ggpht.com/kyli_fpElvyJPO-SmYL6NE4h_5meEj_-3Q3gqzc-DJSkNnPLwl6BCuyllV5yWA=s1200',
+        info: 'De Nachtwacht is het enige schuttersstuk...'
+    }
+]
+
+function display(data){
+
+}
+
+
+function clearData(element){
+    element.innerHTML = "";
+}
--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ window.addEventListener("load",function(){
 	var codes = ["1","2","3","4"];
 
 
+    var artwork = [
+        {
+            id: 3,
+            name: 'Nachtwatch',
+            img_src: 'http://lh3.ggpht.com/kyli_fpElvyJPO-SmYL6NE4h_5meEj_-3Q3gqzc-DJSkNnPLwl6BCuyllV5yWA=s1200',
+            info: 'De Nachtwacht is het enige schuttersstuk...'
+        }
+    ]
 
  $('#reader').html5_qrcode(function(data){
- 	if (data == "3") {
-        	console.log("HUEHUEHUEHUEHUEHUEHUE" + data)
         console.log("Scan Succes! : " + data);
-
-        };
+         document.getElementById("info").innerHTML = artwork[data].name;
     },
     function(error){
         console.log("error : " + error);
