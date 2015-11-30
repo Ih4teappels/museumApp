614f8b512970ffb8253e919aa40a7880c5080994
Ih4teappels <kev10@live.nl>
2015-11-30 10:59:56 +0100

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ window.addEventListener("load",function(){
 
 
  $('#reader').html5_qrcode(function(data){
+ 	if (data == "3") {
+        	console.log("HUEHUEHUEHUEHUEHUEHUE" + data)
         console.log("Scan Succes! : " + data);
+
+        };
     },
     function(error){
         console.log("error : " + error);
